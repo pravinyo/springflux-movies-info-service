785be2f264a66463c0fd2634810848169add313d
Pravin Tripathi <pravinyo12@gmail.com>
2021-12-18 23:53:45 +0530

    simple non blocking API - streaming text

--- src/main/kotlin/com/reactivespring/controller/FluxAndMonoController.kt
+++ src/main/kotlin/com/reactivespring/controller/FluxAndMonoController.kt
@@ -1,9 +1,11 @@
 package com.reactivespring.controller
 
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import java.time.Duration
 
 @RestController
 class FluxAndMonoController {
@@ -17,5 +19,11 @@ class FluxAndMonoController {
     @GetMapping("/mono")
     fun helloWorldMono(): Mono<String> {
         return Mono.just("Hello Pravin").log()
+    }
+
+    @GetMapping("/stream", produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
+    fun stream(): Flux<Long> {
+        return Flux.interval(Duration.ofSeconds(1))
+            .log()
     }
 }
