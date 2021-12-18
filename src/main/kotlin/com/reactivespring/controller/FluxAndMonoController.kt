50ed35d4f36a97da5e84fd1064b1df8aa4d35cea
Pravin Tripathi <pravinyo12@gmail.com>
2021-12-18 23:46:22 +0530

    simple non blocking API - Mono

--- src/main/kotlin/com/reactivespring/controller/FluxAndMonoController.kt
+++ src/main/kotlin/com/reactivespring/controller/FluxAndMonoController.kt
@@ -3,6 +3,7 @@ package com.reactivespring.controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @RestController
 class FluxAndMonoController {
@@ -11,5 +12,10 @@ class FluxAndMonoController {
     fun flux() : Flux<Int> {
 
         return Flux.just(1,2,3).log()
+    }
+
+    @GetMapping("/mono")
+    fun helloWorldMono(): Mono<String> {
+        return Mono.just("Hello Pravin").log()
     }
 }
