29f6433502d42cdf82bcc4f490c0da9ba4e72828
Pravin Tripathi <pravinyo12@gmail.com>
2021-12-19 21:00:50 +0530

    different approach to testing flux

--- src/test/kotlin/unit/com/reactivespring/controller/FluxAndMonoControllerTest.kt
+++ src/test/kotlin/unit/com/reactivespring/controller/FluxAndMonoControllerTest.kt
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
 import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.web.reactive.server.expectBodyList
+import reactor.test.StepVerifier
 
 @WebFluxTest(controllers = [FluxAndMonoController::class])
 @AutoConfigureWebTestClient
@@ -21,7 +23,34 @@ internal class FluxAndMonoControllerTest {
             .exchange()
             .expectStatus()
             .is2xxSuccessful
-            .expectBodyList(Integer::class.java)
+            .expectBodyList(Int::class.java)
             .hasSize(3)
+    }
+
+    @Test
+    fun flux_approach2() {
+        val flux = webTestClient.get().uri("/flux")
+            .exchange()
+            .expectStatus()
+            .is2xxSuccessful
+            .returnResult(Int::class.java)
+            .responseBody
+
+        StepVerifier.create(flux)
+            .expectNext(1,2,3)
+            .verifyComplete()
+    }
+
+    @Test
+    fun flux_approach3() {
+        webTestClient.get().uri("/flux")
+            .exchange()
+            .expectStatus()
+            .is2xxSuccessful
+            .expectBodyList(Int::class.java)
+            .consumeWith<WebTestClient.ListBodySpec<Int>> { entityExchangeResult ->
+                val responseBody = entityExchangeResult.responseBody
+                assert(responseBody!!.size == 3)
+            }
     }
 }
